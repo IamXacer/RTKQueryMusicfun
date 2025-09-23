011e7e2e4c730897e33e73d69d111a12a954c6ad
IamXacer <phanton7487@gmail.com>
2025-09-23 21:16:04 +0200

    Добавлен базовый путь для GitHub Pages

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -4,9 +4,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const playlistsApi = createApi({
   reducerPath: 'playlistsApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
+    baseUrl: "https://musicfun.it-incubator.app/api/1.0/",
     headers: {
-      'API-KEY': import.meta.env.VITE_API_KEY,
+      'API-KEY': "ef7f4357-4b35-41fb-be46-31efc3bd36c5",
     },
   }),
   endpoints: (build) => ({
