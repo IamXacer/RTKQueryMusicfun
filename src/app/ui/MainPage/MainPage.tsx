02fb56792aa9a3390eae40278a76ccc172e35a2e
IamXacer <phanton7487@gmail.com>
2025-12-06 17:22:46 +0300

    Comit2

--- src/app/ui/MainPage/MainPage.tsx
+++ src/app/ui/MainPage/MainPage.tsx
@@ -4,7 +4,7 @@ export const MainPage = () => {
   const {data} = useGetMeQuery()
   return (
     <div color={'red'}>
-      <h1>Main page</h1>
+      <h1>Main page123</h1>
       <div>Login: {data?.login}123</div>
     </div>
   )
