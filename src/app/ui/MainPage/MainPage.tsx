2460d8cb182be87f7ffac379d5dc7ec49979f2c0
IamXacer <phanton7487@gmail.com>
2025-12-06 17:19:51 +0300

    comit1

--- src/app/ui/MainPage/MainPage.tsx
+++ src/app/ui/MainPage/MainPage.tsx
@@ -3,9 +3,9 @@ import { useGetMeQuery } from '@/features/auth/api/authApi.ts'
 export const MainPage = () => {
   const {data} = useGetMeQuery()
   return (
-    <div>
+    <div color={'red'}>
       <h1>Main page</h1>
-      <div>Login: {data?.login}</div>
+      <div>Login: {data?.login}123</div>
     </div>
   )
 }
